c4d4f2043639615c63a161b1d76f5f0957ca414e
lamtiendat111 <76619313+lamtiendat111@users.noreply.github.com>
2021-10-26 20:43:29 +0700

    .

--- Project3/Startup.cs
+++ Project3/Startup.cs
@@ -10,18 +10,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Project3.Models;
 using Project3.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace Project3
 {
     public class Startup
     {
+        private IConfiguration conf;
+
+        public Startup(IConfiguration _conf)
+        {
+            this.conf = _conf;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
             services.AddControllers();
-            services.AddDbContext<DatabaseContext>();
+            var connectString = conf["ConnectionStrings:DefaultConnection"].ToString();
+            services.AddDbContext<DatabaseContext>(option => option.UseLazyLoadingProxies().UseSqlServer(connectString));
+        
             services.AddScoped<AdminService, AdminServiceImp>();
         }
 
