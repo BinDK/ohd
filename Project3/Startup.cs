287c8dbcadad0f698ac57d5952a11dda4cfe50fe
Le Nam Tran <lenamtran1305@gmail.com>
2021-11-04 17:31:07 +0700

    new code

--- Project3/Startup.cs
+++ Project3/Startup.cs
@@ -34,7 +34,8 @@ namespace Project3
             services.AddScoped<AdminService, AdminServiceImp>();
             services.AddScoped<FacilityService, FacilityServiceImp>();
             services.AddScoped<ServiceService, ServiceServiceImp>();
-            services.AddScoped<MyRequestService, MyRequestServiceImp>();
+            services.AddScoped<PriorityService, PriorityServiceImp>();
+            /* services.AddScoped<MyRequestService, MyRequestServiceImp>();*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
