3917dfee6f41bd4eae35b4a8b71932a8b13f83cf
lamtiendat111 <76619313+lamtiendat111@users.noreply.github.com>
2021-11-06 17:24:14 +0700

    sửa gitignore

---
+++ Project3/Services/MyRequestService.cs
@@ -0,0 +1,17 @@
+﻿using Project3.Controllers;
+using Project3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project3.Services
+{
+    public interface MyRequestService
+    {
+        dynamic FindAll();
+        dynamic find(int id);
+        dynamic close(CloseRequest req);
+        object Create(createRequestByUserReq req);
+    }
+}
