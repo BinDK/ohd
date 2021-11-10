1a471067dc8abd6d7cf5652c842655f43eb44805
Le Nam Tran <lenamtran1305@gmail.com>
2021-11-10 16:51:52 +0700

    login

---
+++ Project3/Models/UsernameAndPassword.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project3.Models
+{
+    public class UsernameAndPassword
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
