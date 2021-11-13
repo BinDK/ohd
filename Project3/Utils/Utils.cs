b5acf1454af3fc3277658c1cd9b62c2318eb72ae
lamtiendat111 <76619313+lamtiendat111@users.noreply.github.com>
2021-11-13 23:36:19 +0700

    1

---
+++ Project3/Utils/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Project3.Utils
+{
+    public class Utils
+    {
+        public static void SendMail(string from, string to, string subject, string body, string userName, string password)
+        {
+            MailMessage mailMessage = new MailMessage(from, to, subject, body);
+            mailMessage.BodyEncoding = System.Text.Encoding.UTF8;
+            mailMessage.SubjectEncoding = System.Text.Encoding.UTF8;
+            mailMessage.IsBodyHtml = true;
+
+            using var stmpClient = new SmtpClient("smtp.gmail.com");
+            stmpClient.Port = 587;
+            stmpClient.EnableSsl = true;
+            stmpClient.Credentials = new NetworkCredential(userName, password);
+            try
+            {
+                stmpClient.Send(mailMessage);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
