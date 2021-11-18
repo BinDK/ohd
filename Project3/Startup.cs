e93aeda5be87e128a5eb16514cb8e62e6e2dba5d
Lenamtran1305 <52662774+Lenamtran1305@users.noreply.github.com>
2021-11-18 16:13:02 +0700

    Add files via upload

---
+++ Project3/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project3.Models;
+using Project3.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace Project3
+{
+    public class Startup
+    {
+        private IConfiguration conf;
+
+        public Startup(IConfiguration _conf)
+        {
+            this.conf = _conf;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+            var connectString = conf["ConnectionStrings:DefaultConnection"].ToString();
+            services.AddDbContext<DatabaseContext>(option => option.UseLazyLoadingProxies().UseSqlServer(connectString));
+        
+            services.AddScoped<AdminService, AdminServiceImp>();
+            services.AddScoped<FacilityService, FacilityServiceImp>();
+            services.AddScoped<ServiceService, ServiceServiceImp>();
+            services.AddScoped<PriorityService, PriorityServiceImp>();
+            services.AddScoped<MyRequestService, MyRequestServiceImp>();
+            services.AddScoped<AccountService, AccountServiceImp>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed((host) => true)
+                .AllowCredentials()
+            );
+            app.UseMiddleware<Middleware>();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
