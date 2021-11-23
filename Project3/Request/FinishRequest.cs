9ebebe995df82bb08bc47970df7a286205222d07
Lenamtran1305 <52662774+Lenamtran1305@users.noreply.github.com>
2021-11-23 23:22:55 +0700

    Update FinishRequest.cs

--- Project3/Request/FinishRequest.cs
+++ Project3/Request/FinishRequest.cs
@@ -7,8 +7,12 @@ namespace Project3.Request
 {
     public class FinishRequest
     {
-        public int request_by_user_id { get; set; }
-        public string note { get; set; }
+        public int Id { get; set; }
+        public string Note { get; set; }
+         public string Description { get; set; }
+
+        public int request_status_id { get; set; }
+
 
     }
     //abc
